8639d2958be61b2e778617c977f08cc1ddbb0d90
ms0810 <43068473+kwan-0915@users.noreply.github.com>
2026-06-08 11:38:00 +0800

    update download_finviz

--- main.py
+++ main.py
@@ -12,39 +12,10 @@ root_dir = os.path.join(os.getcwd(), "data")
 if not os.path.exists(root_dir): os.makedirs(root_dir)
 
 custom_headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-    }
-
-def download_finviz():
-    dest_dir = os.path.join(root_dir, "finviz")
-    if not os.path.exists(dest_dir): os.makedirs(dest_dir)
-
-    url = "https://finviz.com/screener"
-
-    response = requests.get(url=url, headers=custom_headers)
-    if response.status_code != 200: raise ValueError("Invalid status code")
-
-    soup = BeautifulSoup(response.text, "html.parser")
-    num_page = soup.find_all("a", {"class": "screener-pages"})[-2].text
-
-    if not num_page.isnumeric(): raise ValueError(f"Invalid page number, {num_page}")
-
-    pages, all_ticker = [i for i in range(1, int(num_page) + 1)], []
-    for i in tqdm(pages, total=len(pages), desc="Downloading Finviz"):
-        try:
-            screener = Screener(pages=[i])
-
-            all_ticker.append(screener.data_frames[i])
-
-        except Exception as e: print(f"Error: {e}, page {i}")
-
-    all_ticker = pd.concat(all_ticker)
-    if not all_ticker.empty:
-        if "No" in all_ticker.columns.tolist(): all_ticker = all_ticker.drop(columns=["No"], axis=1)
-
-        all_ticker.to_csv(os.path.join(dest_dir, f"Finviz_{pd.Timestamp.now().__str__().split(' ')[0]}.csv"), index=False)
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+}
 
 def download_ishares():
     base_url = "https://www.blackrock.com/varnish-api/blk-one01-product-data/product-data/api/v1/get-fund-document?appType=PRODUCT_PAGE&appSubType=ISHARES&targetSite=us-ishares&locale=en_US"
@@ -68,9 +39,62 @@ def download_ishares():
             with open(os.path.join(dest_dir, f"{product}_{pd.Timestamp.now().__str__().split(' ')[0]}.csv"), "wb") as f:
                 f.write(response.content)
 
-        except Exception as e: print(f"[{product}] Error: {e}")
+        except Exception as e:
+            print(f"[{product}] Error: {e}")
+
+def download_finviz():
+    finviz_dir = os.path.join(root_dir, "finviz")
+    if not os.path.exists(finviz_dir): os.makedirs(finviz_dir)
+
+    url = "https://finviz.com/screener"
+
+    response = requests.get(url=url, headers=custom_headers)
+    if response.status_code != 200: raise ValueError(f"Invalid status code, {response.status_code}")
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    num_page = soup.find_all("a", {"class": "screener-pages"})[-2].text
+
+    if not num_page.isnumeric(): raise ValueError(f"Invalid page number, {num_page}")
+
+    pages = [i for i in range(1, int(num_page) + 1)]
+
+    finviz_data = {
+        "overview": [],
+        "valuation": [],
+        "financial": [],
+        "performance": [],
+        "technical": [],
+    }
+
+    finviz_view = {
+        "overview": Screener.ViewOption.OVERVIEW,
+        "valuation": Screener.ViewOption.VALUATION,
+        "financial": Screener.ViewOption.FINANCIAL,
+        "performance": Screener.ViewOption.PERFORMANCE,
+        "technical": Screener.ViewOption.TECHNICAL,
+    }
+
+    for k, v in finviz_data.items():
+        for i in tqdm(pages, total=len(pages), desc=f"[Finviz] Downloading {k.title()}"):
+            try:
+                s = Screener(pages=[i], view_option=finviz_view[k])
+
+                finviz_data[k].append(s.data_frames[i])
+
+            except Exception as e:
+                print(f"[{k.title()}] Error: {e}, page {i}")
+
+        df = pd.concat(finviz_data[k], ignore_index=True, axis=0)
+        if not df.empty:
+            if "No" in df.columns.tolist(): df = df.drop(columns=["No"], axis=1)
+
+            dest_dir = os.path.join(finviz_dir, k.upper())
+            if not os.path.exists(dest_dir): os.makedirs(dest_dir)
+
+            df.to_csv(os.path.join(dest_dir, f"{k.title()}_{pd.Timestamp.now().__str__().split(' ')[0]}.csv"), index=False)
+
 
 if __name__ == "__main__":
-    download_finviz()
-
     download_ishares()
+
+    download_finviz()
